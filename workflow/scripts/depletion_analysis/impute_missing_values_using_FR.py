45109b7f08c4ca742b7eedca4eb9680f94d55526
Yusheng Yang <yangyusheng12323@outlook.com>
2025-06-16 09:57:29 +0800

    Enhance imputation script by adding initial timepoint and cutoff parameters; implement filtering based on second smallest values for improved data quality.

--- workflow/scripts/depletion_analysis/impute_missing_values_using_FR.py
+++ workflow/scripts/depletion_analysis/impute_missing_values_using_FR.py
@@ -39,6 +39,8 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Impute missing values using FR.")
     parser.add_argument("-i", "--input", type=Path, required=True, help="Path to the input file.")
     parser.add_argument("-a", "--annotation", type=Path, required=True, help="Path to the annotation file.")
+    parser.add_argument("-t", "--initial_timepoint", type=str, required=True, help="Initial timepoint.")
+    parser.add_argument("-c", "--cutoff", type=int, required=True, help="Cutoff for the reads number of the initial timepoint.")
     parser.add_argument("-o", "--output", type=Path, required=True, help="Path to the output file.")
     return parser.parse_args()
 
@@ -49,16 +51,36 @@ def main():
     counts_df = pd.read_csv(args.input, index_col=[0, 1, 2, 3], sep="\t", header=[0,1])
     insertion_annotations = pd.read_csv(args.annotation, index_col=[0, 1, 2, 3], sep="\t")
 
+    initial_timepoint_counts_df = counts_df.xs(args.initial_timepoint, axis=1, level="Timepoint")
+
+    # Get the second smallest number per row for filtering
+    second_smallest_per_row = initial_timepoint_counts_df.apply(
+        lambda row: row.nsmallest(2).iloc[-1] if len(row.dropna()) >= 2 else None, 
+        axis=1
+    )
+
+    filtered_initial_timepoint_counts_df = initial_timepoint_counts_df[(initial_timepoint_counts_df.isna().sum(axis=1) < 2) & (second_smallest_per_row >= args.cutoff)]
+
+    filtered_counts_df = counts_df.loc[filtered_initial_timepoint_counts_df.index].copy()
+    
     _, in_gene_insertions = filter_insertions(insertion_annotations)
 
     in_gene_counts_df = counts_df[counts_df.index.isin(in_gene_insertions)].copy()
-    intergenic_counts_df = counts_df[~counts_df.index.isin(in_gene_insertions)].copy().dropna(axis=0, how="any")
+    intergenic_counts_df = filtered_counts_df[~filtered_counts_df.index.isin(in_gene_insertions)].copy().fillna(0)#.dropna(axis=0, how="any")
     print("Insertions with all replicates available in intergenic regions:", intergenic_counts_df.shape[0])
     print("Insertions with at least one replicate available in coding genes:", in_gene_counts_df.shape[0])
     print("Insertions with all replicates available in coding genes:", in_gene_counts_df.dropna(axis=0, how="any").shape[0])
 
     imputed_in_gene_counts_df = impute_missing_values(in_gene_counts_df)
-    imputed_in_gene_counts_df_noNA = imputed_in_gene_counts_df.dropna(axis=0, how="any")
+
+    initial_timepoint_imputed_in_gene_counts_df = imputed_in_gene_counts_df.xs(args.initial_timepoint, axis=1, level="Timepoint")
+    second_smallest_per_row_imputed_in_gene_counts_df = initial_timepoint_imputed_in_gene_counts_df.apply(
+        lambda row: row.nsmallest(2).iloc[-1] if len(row.dropna()) >= 2 else None, 
+        axis=1
+    )
+    filtered_initial_timepoint_imputed_in_gene_counts_df = initial_timepoint_imputed_in_gene_counts_df[(initial_timepoint_imputed_in_gene_counts_df.isna().sum(axis=1) < 2) & (second_smallest_per_row_imputed_in_gene_counts_df >= args.cutoff)]
+    imputed_in_gene_counts_df_noNA = imputed_in_gene_counts_df[imputed_in_gene_counts_df.index.isin(filtered_initial_timepoint_imputed_in_gene_counts_df.index)].fillna(0)
+    # imputed_in_gene_counts_df_noNA = imputed_in_gene_counts_df.dropna(axis=0, how="any")
     print("Insertions with all replicates available in coding genes after imputation:", imputed_in_gene_counts_df_noNA.shape[0])
 
     imputed_counts = pd.concat([intergenic_counts_df, imputed_in_gene_counts_df_noNA], axis=0)
@@ -66,11 +88,11 @@ def main():
     imputed_counts.to_csv(args.output, index=True, sep="\t")
 
     print("### Impute missing values using FR completed ###")
-    insertion_num = counts_df.shape[0]
+    insertion_num = filtered_counts_df.shape[0]
     ingene_num = in_gene_counts_df.shape[0]
-    intergenic_num = counts_df[~counts_df.index.isin(in_gene_insertions)].shape[0]
+    intergenic_num = filtered_counts_df[~filtered_counts_df.index.isin(in_gene_insertions)].shape[0]
 
-    noNA_insertion_num = counts_df.dropna(axis=0, how="any").shape[0]
+    noNA_insertion_num = filtered_counts_df.dropna(axis=0, how="any").shape[0]
     noNA_ingene_num = in_gene_counts_df.dropna(axis=0, how="any").shape[0]
     noNA_intergenic_num = intergenic_counts_df.shape[0]
 
