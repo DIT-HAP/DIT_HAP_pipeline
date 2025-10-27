89846850f04aeeb352ca9a73599c889de9c145f9
Yusheng Yang <yangyusheng12323@outlook.com>
2025-10-27 18:17:16 +0800

    refactor(utils): simplify read_file function by removing unused parameters

--- workflow/src/utils.py
+++ workflow/src/utils.py
@@ -1,26 +1,20 @@
 # ================================ Imports =================================
 from pathlib import Path
-from typing import List, Optional, Dict, Any
 import pandas as pd
-import numpy as np
 
 # ================================= Utility Functions =================================
 def read_file(
     file: Path,
-    index_col: Optional[List[int]] = None,
-    header: Optional[List[int] | str] = "infer",
     **kwargs,
 ) -> pd.DataFrame:
     """Read a file into a pandas DataFrame based on file extension."""
     if "tsv" in file.name:
-        return pd.read_csv(file, index_col=index_col, header=header, sep="\t", **kwargs)
+        return pd.read_csv(file, sep="\t", **kwargs)
     elif "bed" in file.name:
-        return pd.read_csv(file, index_col=index_col, header=header, sep="\t", **kwargs)
+        return pd.read_csv(file, sep="\t", **kwargs)
     elif "csv" in file.name:
-        return pd.read_csv(file, index_col=index_col, header=header, sep=",", **kwargs)
+        return pd.read_csv(file, sep=",", **kwargs)
     elif "xlsx" in file.name:
-        if header == "infer":
-            header = 0
-        return pd.read_excel(file, index_col=index_col, header=header, **kwargs)
+        return pd.read_excel(file, **kwargs)
     else:
         raise ValueError(f"Unsupported file type: {file.name}")
